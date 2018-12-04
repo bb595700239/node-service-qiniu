37a0de5fc59ad9829761d3b54e68f4f6eef530b2
bb595700239 <595700239@qq.com>
2018-12-04 08:35:55 +0800

    修改返回数据格式
    Signed-off-by: bb595700239 <595700239@qq.com>

--- index.js
+++ index.js
@@ -9,25 +9,24 @@ const cors = require('cors');
 app.use(cors());
 
 
-
-var getFn = function(subres,bucket,limit,marker,authorization){
+var getFn = function (subres, bucket, limit, marker, authorization) {
 	var resData = [];
 	var req = http.request({
-		host:"rsf.qbox.me",
+		host: "rsf.qbox.me",
 		port: 80,
 		method: 'GET',
-		path: '/list?bucket='+bucket+'&limit='+limit+'&marker='+marker,
-		headers:{"Authorization":authorization}
-		},function (res) {
+		path: '/list?bucket=' + bucket + '&limit=' + limit + '&marker=' + marker,
+		headers: {"Authorization": authorization}
+	}, function (res) {
 		console.log('STATUS: ' + res.statusCode);
 		console.log('HEADERS: ' + JSON.stringify(res.headers));
 		res.setEncoding('utf8');
 		res.on('data', function (chunk) {
 			resData.push(chunk)
 		});
-		res.on("end", function(){
-			items = resData.join("");
-			subres.json({data:items})
+		res.on("end", function () {
+			items = JSON.parse(resData.join(""));
+			subres.json(items)
 		});
 	});
 	req.on('error', function (e) {
@@ -42,9 +41,9 @@ app.get('/list', (req, res) => {
 	const limit = req.query.limit
 	const marker = req.query.marker
 	const authorization = req.headers.authorization
-	getFn(res,bucket,limit,marker,authorization)
+	getFn(res, bucket, limit, marker, authorization)
 })
 
 app.listen(3001, () => {
-    console.log('正在监听端口3001'); 
+	console.log('正在监听端口3001');
 })
